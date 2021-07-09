168eca7edb0f7b2c7d7e548d1ba0c6bc86264d59
SaraKingGH <60521461+SaraKingGH@users.noreply.github.com>
2021-07-09 13:55:15 -0600

    add script for calling frame 2 video.

---
+++ dataprocessing/frame2video.py
@@ -0,0 +1,19 @@
+import logging
+import argparse
+from pathlib import Path
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+from data_utils import frame2video_convertor
+
+
+if __name__ == "__main__":
+    logger.info(f"breaking the video to frames and save the results")
+    a = argparse.ArgumentParser()
+    a.add_argument("--pathIn", help="path to video folder")
+    a.add_argument("--pathOut", help="saving the video frames based on the number: 0 or based on the frame length (time): 1")
+    a.add_argument("--fps", help="frame per second")
+    args = a.parse_args()
+    args.pathIn = Path('../hockeyTrackingDataset/clips/CAR_VS_BOS_2019/001')
+    args.pathOut = Path('../hockeyTrackingDataset/clips/CAR_VS_BOS_2019/')
+    args.fps = 10
+    frame2video_convertor(args.pathIn, args.pathOut, args.fps)
