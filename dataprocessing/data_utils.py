de103b54721a586d5c92f1a61646308d26c0179b
SaraKingGH <60521461+SaraKingGH@users.noreply.github.com>
2021-07-09 13:55:01 -0600

    add frame to video reconstruction function.

--- dataprocessing/data_utils.py
+++ dataprocessing/data_utils.py
@@ -81,3 +81,32 @@ def videos2frames_frame_number(inputdir: Path, frame_number: int):
                 save_count += 1
                 logger.info(f'frame {count} is saved!')
             count += 1
+
+
+def frame2video_convertor(pathIn: Path, pathOut: Path, fps: int):
+    """
+    combining all the frames to reconstruct the video.
+    """
+    frame_array = []
+    files = list(x for x in pathIn.iterdir() if x.is_file() and x.suffix == '.jpg')
+    logger.info(f'list of files are: {files}')
+    files.sort()
+    frame_array = []
+
+    final_video_name = pathOut / Path('rec_' + pathIn.name + '.mp4')
+    logger.info(f'final video file name is:, {final_video_name}')
+    for i in range(len(files)):
+        filename = files[i] 
+        # read the frame
+        img = cv2.imread(str(filename))
+        height, width, layers = img.shape
+        size = (width, height)
+        # inserting the frames into an image array
+        frame_array.append(img)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(str(final_video_name), fourcc, fps, size)
+
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
