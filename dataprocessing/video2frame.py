27a9a693ce476109cc596a8ab3bf3aa5fdcc8a10
SaraKingGH <60521461+SaraKingGH@users.noreply.github.com>
2021-07-20 10:45:06 -0600

    update path in video2frame.

--- dataprocessing/video2frame.py
+++ dataprocessing/video2frame.py
@@ -10,8 +10,10 @@ if __name__ == "__main__":
     logger.info(f"breaking the video to frames and save the results")
     a = argparse.ArgumentParser()
     a.add_argument("--pathIn", help="path to video folder")
+    a.add_argument("--pathOut", help="path to the output folder")
     a.add_argument("--fnumber", help="frame gap number for saving")
     args = a.parse_args()
-    args.pathIn = Path('../RogersDataset')
-    args.fnumber = 100
-    videos2frames_frame_number(args.pathIn, args.fnumber)
+    args.pathIn = Path('data/Original Videos/')
+    args.pathOut = Path('data/Original Frames/')
+    args.fnumber = 2000
+    videos2frames_frame_number(args.pathIn, args.pathOut, args.fnumber)
